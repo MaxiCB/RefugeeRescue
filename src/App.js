318acb32ee772b4cabe7b9751b00e4ca8613a857
Aaron Beasley <aaroncbrs@gmail.com>
2019-12-23 17:41:26 -0600

    Initial push to GitHub.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
-import { LineLayer } from '@deck.gl/layers';
 import { HeatmapLayer } from '@deck.gl/aggregation-layers';
-import { ScatterplotLayer } from '@deck.gl/layers';
 import { StaticMap } from 'react-map-gl';
 
 import './App.css';
@@ -13,11 +11,7 @@ const mapStyle = 'mapbox://styles/mapbox/dark-v10';
 const countryData = require('./data/countries.json');
 const refugeeData = require('./data/data.json');
 
-let displayData;
-
 const viewState = {
-  // longitude: -122.41669,
-  // latitude: 37.7853,
   longitude: 1,
   latitude: 32,
   zoom: 2,
@@ -25,13 +19,7 @@ const viewState = {
   bearing: 0
 };
 
-// Need to scale data down to single year. Will use the most recent data to eliminate some edge cases.
-
-// Need to implement parsing off the data.json
-// Each item in the data files needs to pass the country name into a function to get the lat and long of country
-// Total can determine the size of the marker
-
-const buildHeat =() => {
+const buildHeat = () => {
   return new HeatmapLayer({
     id: 'heat',
     data: refugeeData,
@@ -41,38 +29,22 @@ const buildHeat =() => {
   })
 }
 
-// This is a dope layer but it was only added for helping clean the data.
-
-// const buildScatter = () => {
-//   return new ScatterplotLayer({
-//     id: 'scatter',
-//     data: refugeeData,
-//     filled: true,
-//     radiusMinPixels: 10,
-//     radiusMaxPixels: 100,
-//     getPosition: d => findCountry(d.country),
-//     pickable: true,
-//     onHover: ({object, x, y}) => {
-//       if(object) {
-//         const { latlng } = object;
-//         console.log(object);
-//       }
-//     }
-//   })
-// }
+const addRandom = () => {
+  let rand = Math.random() * (1 - 0.02) + 0.01;
+  return rand;
+}
 
 const findCountry = (refugeeCountry) => {
   let country = countryData.find(item => item.name === refugeeCountry)
   if(country != undefined){
-    console.log(country.latlng[1], country.latlng[0]);
-    return [country.latlng[1], country.latlng[0]];
+    console.log(country.latlng[1] + addRandom(), country.latlng[0] + addRandom());
+    return [country.latlng[1] + addRandom(), country.latlng[0] + addRandom()];
   }
 }
 
 function App() {
   return (
-    <DeckGL initialViewState={viewState} height='100%' width='100%' controller={true} layers={buildHeat()}>
-       {/* <DeckGL initialViewState={viewState} height='100%' width='100%' controller={true} layers={flight}> */}
+    <DeckGL initialViewState={viewState} controller={true} layers={buildHeat()}>
         <StaticMap mapStyle={mapStyle} mapboxApiAccessToken={mapBoxToken}/>
       </DeckGL>
   );
