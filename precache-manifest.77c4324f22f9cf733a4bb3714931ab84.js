848b48a31c4ed5bb52436017f39ade53dca3723b
Aaron Beasley <aaroncbrs@gmail.com>
2019-12-23 17:56:40 -0600

    Updates

---
+++ precache-manifest.77c4324f22f9cf733a4bb3714931ab84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626cd609e1bed7322185972de5630655",
+    "url": "/RefugeeRescue/index.html"
+  },
+  {
+    "revision": "2f8346d17f4fa15e5fb8",
+    "url": "/RefugeeRescue/static/css/main.fd29a41e.chunk.css"
+  },
+  {
+    "revision": "993fd56ef456a2da19d2",
+    "url": "/RefugeeRescue/static/js/2.6eb28b3d.chunk.js"
+  },
+  {
+    "revision": "bdc7727bc0f16ac349efac0552ddc4e5",
+    "url": "/RefugeeRescue/static/js/2.6eb28b3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f8346d17f4fa15e5fb8",
+    "url": "/RefugeeRescue/static/js/main.f9fa4b00.chunk.js"
+  },
+  {
+    "revision": "096fee33aac471322b66",
+    "url": "/RefugeeRescue/static/js/runtime-main.262d1cd3.js"
+  }
+]);
